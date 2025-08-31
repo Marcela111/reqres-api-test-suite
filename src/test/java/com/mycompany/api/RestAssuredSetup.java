6356539e493f9eb5139091d31000725477312e84
Marcela111 <mamorita15@gmail.com>
2025-08-31 00:47:32 -0400

    feat: Add API key configuration and setup

--- src/test/java/com/mycompany/api/RestAssuredSetup.java
+++ src/test/java/com/mycompany/api/RestAssuredSetup.java
@@ -3,25 +3,25 @@ package com.mycompany.api;
 import org.junit.jupiter.api.BeforeAll;
 
 import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
 
 /**
- * Test setup class to configure Rest Assured with the base URI and API key.
- * This runs once before all tests in the test suite.
+ * Clase de configuración de prueba para configurar Rest Assured.
+ * Esto se ejecuta una vez antes de todas las pruebas en la suite de pruebas.
  */
 public class RestAssuredSetup {
 
     @BeforeAll
     public static void setup() {
-        // Updated the baseURI to include the "/api" path for reqres.in
+        // Se ha actualizado la URI base para incluir la ruta "/api"
         RestAssured.baseURI = "https://reqres.in/api";
-        // Use the ConfigurationManager class to get the API key
-        // String apiKey = ConfigurationManager.getApiKey();
+        // Usa la clase ConfigurationManager para obtener la clave de API (si es necesario)
+        String apiKey = ConfigurationManager.getApiKey();
 
-        // if (apiKey != null) {
-        //     // Agrega la clave de API al encabezado de las solicitudes
-        //     // Reemplaza "Authorization" y "Bearer " si la documentación de la API especifica algo diferente
-        //     // Esta línea está comentada porque la API de reqres.in no usa una clave de API.
-        //     // RestAssured.requestSpecification = RestAssured.given().header("Authorization", "Bearer " + apiKey);
-        // }
+        if (apiKey != null) {
+            // Agrega la clave de API al encabezado de las solicitudes
+            // Reemplaza "Authorization" y "Bearer " si la documentación de la API especifica algo diferente
+            RestAssured.requestSpecification = given().header("Authorization", "Bearer " + apiKey);
+        }
     }
 }
