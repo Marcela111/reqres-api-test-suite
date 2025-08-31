877e6c6c8abf2963e72f51ade5d259c63bb672e3
Marcela111 <mamorita15@gmail.com>
2025-08-31 00:26:04 -0400

    feat: Add API key configuration and setup

--- src/test/java/com/mycompany/api/RestAssuredSetup.java
+++ src/test/java/com/mycompany/api/RestAssuredSetup.java
@@ -1,20 +1,26 @@
 package com.mycompany.api;
 
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
+import io.restassured.RestAssured;
+
+/**
+ * Test setup class to configure Rest Assured with the base URI and API key.
+ * This runs once before all tests in the test suite.
+ */
 public class RestAssuredSetup {
 
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = "https://reqres.in";
-        // Usa la clase ConfigurationManager para obtener la clave de API
+        // Use the ConfigurationManager class to get the API key
         String apiKey = ConfigurationManager.getApiKey();
 
         if (apiKey != null) {
             // Agrega la clave de API al encabezado de las solicitudes
             // Reemplaza "Authorization" y "Bearer " si la documentación de la API especifica algo diferente
-            RestAssured.requestSpecification = RestAssured.given().header("Authorization", "Bearer " + apiKey);
+            // Esta línea está comentada porque la API de reqres.in no usa una clave de API.
+            // RestAssured.requestSpecification = RestAssured.given().header("Authorization", "Bearer " + apiKey);
         }
     }
 }
